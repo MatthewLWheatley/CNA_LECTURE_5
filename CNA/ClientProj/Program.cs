db6df45fe520842674c6c13f9c13c83d79c86e07
MatthewLWheatley <76147911+MatthewLWheatley@users.noreply.github.com>
2022-11-09 12:48:27 +0000

    Fixed Frame Work

---
+++ CNA/ClientProj/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ClientProj
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+
+    public class Client 
+    {
+        private TcpClient m_TcpClient;
+        private NetworkStream m_Stream;
+        private StreamWriter m_Wirter;
+        private StreamReader m_Reader;
+
+        public Client() 
+        { 
+        
+        }
+
+        public bool Connect(string ipAddress, int pool) 
+        {
+            return false;
+        }
+
+        public void Run() 
+        { 
+        
+        }
+
+        private void ProccessServerResponse() 
+        { 
+        
+        }
+    }
+}
---
+++ CNA/ServerProj/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ServerProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("127.0.0.1", 4444);
+            server.Start();
+            server.Stop();
+        }
+    }
+
+    public class Server
+    {
+        private static TcpListener m_TcpListener;
+
+        public Server(string ipAdress, int port)
+        {
+            IPAddress ip = IPAddress.Parse(ipAdress);
+            m_TcpListener = new TcpListener(ip, port);
+        }
+
+        public void Start()
+        {
+            m_TcpListener.Start();
+            Socket socket = m_TcpListener.AcceptSocket();
+            ClientMethod(socket);
+        }
+
+        public void Stop()
+        {
+            m_TcpListener.Stop();
+        }
+
+        private void ClientMethod(Socket socket)
+        {
+            string receivedMessage = "";
+            NetworkStream stream = new NetworkStream(socket,true);
+
+            StreamReader streamReader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter streamWriter = new StreamWriter(stream, Encoding.UTF8);
+
+            streamWriter.WriteLine("You Have connected to the STREAM");
+            streamWriter.Flush();
+
+            while ((receivedMessage = streamReader.ReadLine()) != null)
+            {
+                GetReturnMessage(receivedMessage);
+                streamWriter.WriteLine(receivedMessage);
+                streamWriter.Flush();
+                if (receivedMessage == "end") 
+                {
+                    break;
+                }
+                socket.Close();
+            }
+
+        }
+
+        private string GetReturnMessage(string code) 
+        {
+            if (code == "hi") 
+            {
+                code = "hello";
+            }
+            return code;
+        }
+    }
+}
