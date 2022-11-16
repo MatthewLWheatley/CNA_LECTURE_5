4beace5808c4a2d133fd64cc0f074a6bbbfe4374
MatthewLWheatley <76147911+MatthewLWheatley@users.noreply.github.com>
2022-11-16 12:52:39 +0000

    multiple Clinets
    beinging of implimenation of multiple clients

--- CNA/ClientProj/Program.cs
+++ CNA/ClientProj/Program.cs
@@ -13,7 +13,9 @@ namespace ClientProj
     {
         public static void Main()
         {
-            Console.WriteLine("hello world");
+            Client client = new Client();
+            if (client.Connect("127.0.0.1", 4444)) client.Run();
+            else Console.WriteLine("Faild to connect to server");
         }
     }
 
@@ -21,38 +23,55 @@ namespace ClientProj
     {
         private TcpClient m_TcpClient;
         private NetworkStream m_Stream;
-        private StreamWriter m_Wirter;
+        private StreamWriter m_Writer;
         private StreamReader m_Reader;
 
-        public IPAddress IP { get; set; }
+        public int Port { get; set; }
 
         public Client() 
         {
             m_TcpClient = new TcpClient();
-            try
-            {
-                
-                m_TcpClient.Connect();
-            }
-            catch 
-            { 
-                
-            }
+            
         }
 
         public bool Connect(string ipAddress, int port) 
         {
-            return false;
+            try
+            {
+                m_TcpClient.Connect("127.0.0.1", 4444);
+                m_Stream = m_TcpClient.GetStream();
+                m_Reader = new StreamReader(m_Stream, Encoding.UTF8);
+                m_Writer = new StreamWriter(m_Stream, Encoding.UTF8);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
         }
 
         public void Run() 
-        { 
-        
+        {
+            string userinput = null;
+            ProccessServerResponse();
+            while ((userinput = Console.ReadLine()) != null) 
+            {
+                m_Writer.WriteLine(userinput);
+                m_Writer.Flush();
+                ProccessServerResponse();
+                if (userinput == "exit") 
+                {
+                    break;
+                }
+            }
+            m_TcpClient.Close();
+
         }
 
         private void ProccessServerResponse() 
-        { 
-        
+        {
+            Console.WriteLine("server says: " + m_Reader.ReadLine() + "\n");
         }
     }
 }
--- CNA/ServerProj/Program.cs
+++ CNA/ServerProj/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.IO;
+using System.Collections.Concurrent;
 
 namespace ServerProj
 {
@@ -23,6 +24,8 @@ namespace ServerProj
     {
         private static TcpListener m_TcpListener;
 
+        ConcurrentDictionary<int, ConnectedClient> m_Clients;
+
         public Server(string ipAdress, int port)
         {
             IPAddress ip = IPAddress.Parse(ipAdress);
@@ -31,9 +34,15 @@ namespace ServerProj
 
         public void Start()
         {
+            m_Clients = new ConcurrentDictionary<int, ConnectedClient>();
+            int clientIndex = 0, connectionLimit = 4;
             m_TcpListener.Start();
-            Socket socket = m_TcpListener.AcceptSocket();
-            ClientMethod(socket);
+            while (true) 
+            { 
+                
+            }
+            //Socket socket = m_TcpListener.AcceptSocket();
+            
         }
 
         public void Stop()
@@ -41,38 +50,86 @@ namespace ServerProj
             m_TcpListener.Stop();
         }
 
-        private void ClientMethod(Socket socket)
+        //private void ClientMethod(Socket socket)
+        //{
+        //    string receivedMessage = "";
+        //    NetworkStream stream = new NetworkStream(socket,true);
+
+        //    StreamReader streamReader = new StreamReader(stream, Encoding.UTF8);
+        //    StreamWriter streamWriter = new StreamWriter(stream, Encoding.UTF8);
+
+        //    streamWriter.WriteLine("You Have connected to the STREAM");
+        //    streamWriter.Flush();
+
+        //    while ((receivedMessage = streamReader.ReadLine()) != null)
+        //    {
+        //        receivedMessage = GetReturnMessage(receivedMessage);
+        //        streamWriter.WriteLine(receivedMessage);
+        //        streamWriter.Flush();
+        //        if (receivedMessage == "end") 
+        //        {
+        //            break;
+        //        }
+        //    }
+
+        //    socket.Close();
+        //}
+
+        private string GetReturnMessage(string code)
         {
-            string receivedMessage = "";
-            NetworkStream stream = new NetworkStream(socket,true);
-
-            StreamReader streamReader = new StreamReader(stream, Encoding.UTF8);
-            StreamWriter streamWriter = new StreamWriter(stream, Encoding.UTF8);
-
-            streamWriter.WriteLine("You Have connected to the STREAM");
-            streamWriter.Flush();
-
-            while ((receivedMessage = streamReader.ReadLine()) != null)
-            {
-                GetReturnMessage(receivedMessage);
-                streamWriter.WriteLine(receivedMessage);
-                streamWriter.Flush();
-                if (receivedMessage == "end") 
-                {
-                    break;
-                }
-                socket.Close();
-            }
-
-        }
-
-        private string GetReturnMessage(string code) 
-        {
-            if (code == "hi") 
+            if (code == "hi")
             {
                 code = "hello";
             }
             return code;
+        }
+    }
+
+    public class ConnectedClient 
+    {
+        Socket m_socket;
+        NetworkStream m_Stream;
+        StreamReader m_Reader;
+        StreamWriter m_Writer;
+        object m_ReadLock;
+        object m_WriteLock;
+
+        public ConnectedClient(Socket socket) 
+        { 
+            m_WriteLock = new object();
+            m_ReadLock = new object();
+
+            m_socket = socket;
+
+            m_Stream = new NetworkStream(socket, true);
+
+            m_Reader = new StreamReader(m_Stream, Encoding.UTF8);
+            m_Writer = new StreamWriter(m_Stream, Encoding.UTF8);
+        }
+
+        public void Close() 
+        { 
+            m_Stream.Close();
+            m_Reader.Close();
+            m_Writer.Close();
+            m_socket.Close();
+        }
+
+        public string Read() 
+        {
+            lock (m_ReadLock) 
+            { 
+                return m_Reader.ReadLine();
+            }
+        }
+
+        public void Send(string message) 
+        {
+            lock (m_WriteLock) 
+            {
+                m_Writer.WriteLine(message);
+                m_Writer.Flush();
+            }
         }
     }
 }
