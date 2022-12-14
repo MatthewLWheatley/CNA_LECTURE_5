437b84da9752615405158d8a4c7d341e6f192de6
MatthewLWheatley <76147911+MatthewLWheatley@users.noreply.github.com>
2022-12-14 10:59:27 +0000

    Packets i think

---
+++ CNA/Packets/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+
+namespace Packets
+{
+    enum PacketType
+    {
+        ChatMessage,
+        PrivateMessage,
+        CleintName
+    }
+
+    [Serializable]
+    public class Packet
+    {
+        PacketType packetType
+        {
+            get
+            {
+                return packetType;
+            }
+            set
+            {
+                packetType = value;
+            }
+        }
+    }
+
+    [Serializable]
+    class ChatMessagePacket : Packet
+    {
+        public string message;
+
+        public ChatMessagePacket(string message)
+        {
+            this.message = message;
+        }
+    }
+}
